9379507220b46b0df279e961d557ef1a5c8ea647
林柏强 <lin.boqiang@ucsmy.com>
2017-03-06 09:17:49 +0800

    Test

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/** 
+ * @ClassName: Test 
+ * @Description: TODO(Simple Class) 
+ * @author (Mike)  
+ * @date Mar 6, 2017 9:14:22 AM 
+ * @version V1.0 
+ */
+public class Test {
+
+}
